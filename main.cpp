de594f67241ee690afc9be9576030da7dc3f76bb
khei4 <kk.asano.luxy@gmail.com>
2023-02-18 22:04:30 +0900

    compile: 1 integer
    fix tests
    
    target not found
    
    initialize target registroy
    
    refacotring
    
    chore: unneccesarry initialization
    
    chore

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Instructions.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Support/TargetSelect.h"
 #include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
+#include <algorithm>
 #include <memory>
+#include <utility>
 using namespace llvm;
 
 static std::unique_ptr<LLVMContext> TheContext;
@@ -19,6 +24,21 @@ static void InitializeModule() {
   TheContext = std::make_unique<LLVMContext>();
   TheModule = std::make_unique<Module>("top", *TheContext);
 
+  // Initialize the target registry etc.
+  InitializeNativeTarget();
+  auto TargetTriple = sys::getDefaultTargetTriple();
+  TheModule->setTargetTriple(TargetTriple);
+  std::string Error;
+  auto Target = TargetRegistry::lookupTarget(TargetTriple, Error);
+  if (!Target) {
+    errs() << Error;
+    exit(1);
+  }
+  TargetOptions opt;
+  auto RM = Optional<Reloc::Model>();
+  auto TargetMachine = Target->createTargetMachine(
+      TargetTriple, /*CPU=*/"generic", /*Features=*/"", opt, RM);
+  TheModule->setDataLayout(TargetMachine->createDataLayout());
   // Create a new builder for the module.
   Builder = std::make_unique<IRBuilder<>>(*TheContext);
 }
@@ -27,12 +47,13 @@ int main(int argc, char **argv) {
     fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
     return 1;
   }
+  char *p = argv[1];
   InitializeModule();
   Function *main = Function::Create(
       FunctionType::get(Type::getInt32Ty(*TheContext), false),
       Function::ExternalLinkage, Twine("main"), TheModule.get());
   Builder->SetInsertPoint(BasicBlock::Create(*TheContext, "", main));
-  Builder->CreateRet(
-      ConstantInt::get(*TheContext, APInt(32, atoi(argv[1]), true)));
+  Builder->SetInsertPoint(BasicBlock::Create(*TheContext, "second", main));
+  Builder->CreateRet(Builder->getInt32(atoi(argv[1])));
   TheModule->print(outs(), nullptr);
 }
