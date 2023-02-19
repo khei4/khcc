57330588d56ef6a8cd1a61a869cc3f8321c68fab
khei4 <kk.asano.luxy@gmail.com>
2023-02-19 22:05:21 +0900

    tmp

---
+++ khcc.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "llvm/ADT/APInt.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
+#include <algorithm>
+#include <memory>
+#include <stack>
+#include <utility>
+using namespace llvm;
